4e7c231eee045d9f96bc167d22f8e7418a2e8dd0
Grady Zhuo <gradyzhuo@gmail.com>
2016-03-22 23:19:31 +0800

    first commit.

---
+++ AcclaimOC/AcclaimOC.h
@@ -0,0 +1,19 @@
+//
+//  AcclaimOC.h
+//  AcclaimOC
+//
+//  Created by Grady Zhuo on 3/22/16.
+//  Copyright © 2016 Offsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AcclaimOC.
+FOUNDATION_EXPORT double AcclaimOCVersionNumber;
+
+//! Project version string for AcclaimOC.
+FOUNDATION_EXPORT const unsigned char AcclaimOCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AcclaimOC/PublicHeader.h>
+
+
